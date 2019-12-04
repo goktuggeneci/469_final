1015c9dfff2696d8ee5e4852a24f59b769001132
Goktug Geneci <goktuggeneci@gmail.com>
2019-12-04 14:12:14 -0500

    completed project with report

--- untitled/image_seg.py
+++ untitled/image_seg.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 #pic = plt.imread('interlocking_ub.png')/255  # dividing by 255 to bring the pixel values between 0 and 1
-img = mpimg.imread('..\interlocking_ub.png')
+img = mpimg.imread('..\grass-tiger.png')
 #print(img)
 #print(img.shape)
 pic_n = img.reshape(img.shape[0]*img.shape[1], img.shape[2])
 
 
 
-kmeans = KMeans(n_clusters=3, random_state=0).fit(pic_n)
+kmeans = KMeans(n_clusters=8, random_state=0).fit(pic_n)
 pic2show = kmeans.cluster_centers_[kmeans.labels_]
 
 cluster_pic = pic2show.reshape(img.shape[0], img.shape[1], img.shape[2])
+print("Done")
 plt.imshow(cluster_pic)
 plt.show()
